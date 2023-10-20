c31e4850c897afbb2077a9ed1fd57381b788b3b8
Eddie Sandler <ed.sandler@gmail.com>
2023-10-20 13:00:26 -0400

    now utilizing sessions and flash messages

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask,render_template,flash,request,session,redirect
+
 from surveys import satisfaction_survey as survey
 
 
 app=Flask(__name__)
-responses =[]
+
+responses=[]
+
 
 
 app.config['SECRET_KEY'] = 'itsasekrit'
 
 @app.route("/")
 def home():
-
+    #global responses
+    responses.clear()
+    session['key'] = responses
     return render_template('start_survey.html',survey=survey)
 
 @app.route('/begin', methods=['POST'])
@@ -24,23 +29,31 @@ def get_question(id):
 
     question = survey.questions[id]
 
-
-
-
     return render_template('question.html',question=question,id=id)
 
 @app.route('/answer',methods=['POST'])
 def get_answer():
+    #global responses
 
     choice = request.form['answer']
 
     responses.append(choice)
-    l1= {len(responses)}
-    l2={len(survey.questions)}
+    l1= len(responses)
+    l2=len(survey.questions)
 
     if l1 ==l2:
-        message=flash("Thanks for Taking the Survey")
-        return render_template('complete_survey.html',answers=responses,message=message,survey=survey)
+
+
+
+        return redirect('/survey_completed')
     else:
 
-        return redirect(f"question/{len(responses)}")
+        return redirect(f"question/{l1}")
+
+    #https://p.ip.fi/5zYV
+
+@app.route('/survey_completed/')
+def end_survey():
+    message=flash("Thanks for Taking the Survey")
+
+    return render_template('complete_survey.html',responses= responses,message=message,survey=survey)
